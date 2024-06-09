a871b1b21d3bd0aa9519473d18b640ea7b08ac4a
Carlos Ash <car.ferreira.dv@gmail.com>
2024-06-09 20:42:07 -0300

    shopping cart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "BRL" | "USD" | "EUR",
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+  const {currency = "BRL", notation = "compact"} = options
+
+  const numericPrice = 
+    typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice)
+}
